b7f9aa0f3671876b1af82f14c5f44679030598d8
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2024-12-15 17:54:25 +0000

    Initial prototype

---
+++ api/mod.ts
@@ -0,0 +1,31 @@
+import { basename, dirname, extname, join } from "jsr:@std/path@^1.0.8";
+
+export function createFile(content: string | object, path: string | undefined = undefined) {
+    let output: string | undefined = undefined;
+    
+    if (typeof content === "string") {
+        output = content;
+    }
+    else {
+        output = JSON.stringify(content);
+    }
+
+    let outputPath = path;
+
+    if (outputPath === undefined) {
+        const entryPoint = Deno.mainModule.replace("file:///", "");
+        const baseName = basename(entryPoint, extname(entryPoint));
+        const relativePath = dirname(entryPoint.split("/.regolith/tmp/")[1]);
+        outputPath = join(Deno.cwd(), "../.regolith/tmp/", relativePath, baseName + ".json");
+    }
+    else {
+        outputPath = join(Deno.cwd(), "../.regolith/tmp/", outputPath);
+    }
+
+    console.log(outputPath);
+
+    const outputDir = dirname(outputPath);
+    Deno.mkdirSync(outputDir, { recursive: true });
+    Deno.writeTextFileSync(outputPath, output, { create: true });
+    console.log(outputPath);
+}
---
+++ src/Index.ts
@@ -0,0 +1,35 @@
+import { join } from "jsr:@std/path";
+import { walkSync } from "jsr:@std/fs";
+
+const ROOT_DIR = Deno.env.get("ROOT_DIR")!;
+
+const DIRECTORIES = [
+    join(Deno.cwd(), "BP"),
+    join(Deno.cwd(), "RP")
+]
+
+const tsFiles: string[] = [];
+
+for (const dir of DIRECTORIES) {
+    for (const entry of walkSync(dir, { exts: [".ts"], includeFiles: true })) {
+        tsFiles.push(entry.path);
+    }
+}
+
+async function runScript(filePath: string) {
+    const process = new Deno.Command("deno", {
+        args: ["run", "--unstable-sloppy-imports", "--allow-all", filePath],
+        stdout: "inherit",
+        stdin: "inherit",
+        cwd: join(ROOT_DIR, "packs")
+    }).spawn();
+
+    const status = await process.status;
+
+    if (!status.success) {
+        console.error(`❌ Failed to run: ${filePath}`)
+    }
+}
+
+await Promise.all(tsFiles.map(runScript));
+await Promise.all(tsFiles.map((path) => Deno.remove(path)));
