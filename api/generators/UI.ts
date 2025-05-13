867bc1886c39810d69223ee9d9fdcd2c04bf2385
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-05-13 22:11:38 +0100

    move backend into generators

--- api/generators/UI.ts
+++ api/generators/UI.ts
@@ -4,7 +4,7 @@ import { GeneratorBase } from "../GeneratorBase.ts";
 
 type Constructor<T = {}> = new (...args: any[]) => T;
 type VariableExpr = string;
-type Variable<T> = T | VariableExpr;
+export type Variable<T> = T | VariableExpr;
 
 export type Size = Variable<number> | `${number}` | `${number}px` | `${number}%${'x' | 'y' | '' | 'c' | 'cm' | 'sm'}` | `fill` | `default`;
 export type Size2D = [Size, Size];
@@ -261,7 +261,16 @@ interface CollectionComponentProps {
  * Props that are used within the UI generator. Not found in minecraft
  */
 interface GeneratorProps {
+    /**
+     * A flag that indicates if the generator should handle children elements
+     * - if true, the generator will ignore the children elements and only generate the parent element
+     */
     handles_children?: true;
+
+    /**
+     * Can be used to define fallback values for variables
+     */
+    defaults?: Record<string, Variable<any>>;
 }
 
 // Components
@@ -324,7 +333,7 @@ function CollectionComponent<TBase extends Constructor<Control>>(Base: TBase) {
 // Element type props
 export interface PanelProps extends LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
 export interface ImageProps extends SpriteComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
-export interface LabelProps extends LabelComponentProps, LayoutComponentProps, ControlProps, GeneratorProps {};
+export interface LabelProps extends LabelComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
 export interface CustomProps extends CustomRendererComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
 export interface StackPanelProps extends StackPanelComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, CollectionComponentProps, GeneratorProps {};
 export interface CollectionProps extends CollectionComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
@@ -370,4 +379,57 @@ export class Collection extends CollectionComponent(DataBindingComponent(LayoutC
         super();
         this.setType("collection");
     }
+}
+
+// React implementation
+import React from "npm:react";
+
+const UI_PROPS = ["panel","input_panel","image","grid","label","button","screen","scroll_box","stack_panel","toggle","slider","slider_box","dropdown","edit_box","custom","scroll_view","scroll_track","factory","selection_wheel","grid_page_indicator","label_cycler","image_cycler","collection_panel","controls","variables","visible","ignored","modifications","anims","disable_anim_fast_forward","animation_reset_name","enabled","layer","alpha","propagate_alpha","clips_children","allow_clipping","clip_offset","clip_state_change_event","enabled_scissor_test","property_bag","selected","use_child_anchors","grid_position","debug","anchor_from","anchor_to","contained","draggable","follows_cursor","offset","size","max_size","min_size","inherit_max_sibling_width","inherit_max_sibling_height","use_anchored_offset","bindings","sound_name","sound_pitch","sound_volume","default_focus_precedence","focus_enabled","focus_wrap_enabled","focus_magnet_enabled","focus_identifier","focus_change_down","focus_change_up","focus_change_left","focus_change_right","focus_mapping","focus_container","use_last_focus","focus_navigation_mode_left","focus_navigation_mode_right","focus_navigation_mode_down","focus_navigation_mode_up","focus_container_custom_left","focus_container_custom_right","focus_container_custom_down","focus_container_custom_up","button_mappings","modal","inline_modal","always_listen_to_input","always_handle_pointer","always_handle_controller_direction","hover_enabled","prevent_touch_input","consume_event","consume_hover_events","gesture_tracking_button","texture","uv","uv_size","nineslice_size","base_size","color","tiled","tiled_scale","clip_direction","clip_ratio","clip_pixelperfect","keep_ratio","bilinear","fill","grayscale","force_texture_reload","$fit_to_witdh","zip_folder","texture_file_system","allow_debug_missing_texture","pixel_perfect","grid_dimensions","maximum_grid_items","grid_dimension_binding","grid_rescaling_type","grid_fill_direction","precached_grid_item_count","grid_item_template","text","locked_color","shadow","font_size","font_scale_factor","localize","line_padding","font_type","backup_font_type","text_alignment","hide_hypen","locked_alpha","enable_profanity_filter","notify_on_ellipses","default_control","hover_control","pressed_control","locked_control","render_only_when_topmost","screen_not_flushable","always_accepts_input","render_game_behind","absorbs_input","is_showing_menu","is_modal","should_steal_mouse","low_frequency_rendering","screen_draws_last","vr_mode","force_render_below","send_telemetry","close_on_player_hurt","cache_screen","load_screen_immediately","gamepad_cursor","gamepad_cursor_deflection_mode","should_be_skipped_during_automation","orientation","radio_toggle_group","toggle_name","toggle_default_state","toggle_group_forced_index","toggle_group_default_selected","reset_on_focus_lost","toggle_on_button","toggle_off_button","enable_directional_toggling","toggle_grid_collection_name","checked_control","unchecked_control","checked_hover_control","unchecked_hover_control","checked_locked_control","unchecked_locked_control","checked_locked_hover_control","unchecked_locked_hover_control","slider_track_button","slider_small_decrease_button","slider_small_increase_button","slider_steps","slider_direction","slider_timeout","slider_collection_name","slider_name","slider_select_on_hover","slider_selected_button","slider_deselected_button","slider_box_control","background_control","background_hover_control","progress_control","progress_hover_control","indent_control","dropdown_name","dropdown_content_control","dropdown_area","text_box_name","text_edit_box_grid_collection_name","constrain_to_rect","enabled_newline","text_type","max_length","text_control","place_holder_control","can_be_deselected","always_listening","virtual_keyboard_buffer_control","renderer","scrollbar_track_button","scrollbar_touch_button","scroll_speed","gesture_control_enabled","always_handle_scrolling","touch_mode","scrollbar_box","scrollbar_track","scroll_view_port","scroll_content","scroll_box_and_track_panel","jump_to_bottom_on_update","allow_scroll_even_when_content_fits","inner_radius","outer_radius","state_controls","slice_count","button_name","iterate_left_button_name","iterate_right_button_name","initial_button_slice","grid_item_when_current","grid_item_when_not_current","cycler_manager_size_control_target","target_cycler_to_compare","next_sub_page_button_name","prev_sub_page_button_name","text_labels","images", "collection_name"];
+
+export function createMinecraftElement(type: Function, props: any, ...children: any[]): Control {
+    props ??= {};
+
+    const handlesChildren = props.handles_children;
+
+    // Inject children into props, this is useful for components that manage their own children
+    if (children.length > 0) {
+        props.children = children.length === 1 ? children[0] : children;
+    }
+
+    const element = new (type as any)(props);
+
+    for (const [key, value] of Object.entries(props ?? {})) {
+        if (key === "children") continue;
+
+        if (typeof (element as any)[key] === "function") {
+            (element as any)[key](value);
+        } 
+        else if (key.startsWith("$") || UI_PROPS.includes(key)) {
+            element.data[key] = value;
+        }
+    }
+
+    // Set any default values
+    if (props.defaults) {
+        for (const [key, value] of Object.entries(props.defaults)) {
+            // Don't set default values of existing props
+            if (element.data[key] !== undefined) continue;
+            element.data[`${key}|default`] = value;
+        }
+    }
+    
+
+    if (!handlesChildren && children.length && typeof (element as any).addControl === "function") {
+        children.flat().forEach((child, i) => {
+            if (child instanceof Control) {
+                element.addControl(`${i}`, child);
+            }
+        })
+    }
+
+    return element;
+}
+
+export function createMinecraftFragment(...children: any[]) {
+  return children;
 }
