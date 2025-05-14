34e68cc7e2c02ab6953482eb4c70ed3ef5b4ee46
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-05-14 20:31:39 +0100

    Add: Bunch of components and fixed bug with non flattened children

--- api/generators/UI.ts
+++ api/generators/UI.ts
@@ -129,6 +129,10 @@ export class Control extends GeneratorBase<Control> {
     public addControl(name: string, control: Control): this {
         const base = control.inheritedControl;
 
+        if (!(control instanceof Control)) {
+            throw new Error("Control must be an instance of Control.");
+        }
+
         if (base != undefined) {
             this.controls.push([`${name}@${base.owningNamespace}.${base.controlName}`, control]);
         }
@@ -229,9 +233,17 @@ interface SpriteComponentProps {
 
 interface LayoutComponentProps {
     size?: Variable<Size2D>;
+    max_size?: Variable<Size2D>;
+    min_size?: Variable<Size2D>;    
     offset?: Variable<Size2D>;
     anchor_to?: Variable<Anchor>;
     anchor_from?: Variable<Anchor>;
+    inherit_max_sibling_width?: Variable<boolean>;
+    inherit_max_sibling_height?: Variable<boolean>;
+    use_anchored_offset?: Variable<boolean>;
+    contained?: Variable<boolean>;
+    draggable?: Variable<boolean>;
+    follows_cursor?: Variable<boolean>;
 }
 
 interface ControlProps {
@@ -270,6 +282,80 @@ interface CollectionComponentProps {
      * Name of the collection to be used
      */
     collection_name?: Variable<string>;
+}
+
+
+interface InputComponentProps {
+    button_mappings?: Variable<any>;
+    modal?: Variable<boolean>;
+    inline_modal?: Variable<boolean>;
+    always_listen_to_input?: Variable<boolean>;
+    always_handle_pointer?: Variable<boolean>;
+    always_handle_controller_direction?: Variable<boolean>;
+    hover_enabled?: Variable<boolean>;
+    prevent_touch_input?: Variable<boolean>;
+    consume_event?: Variable<boolean>;
+    consume_hover_events?: Variable<boolean>;
+    gesture_tracking_button?: Variable<string>;
+}
+
+interface FocusComponentProps {
+    default_focus_precedence?: Variable<number>;
+    focus_enabled?: Variable<boolean>;
+    focus_wrap_enabled?: Variable<boolean>;
+    focus_magnet_enabled?: Variable<boolean>;
+    focus_identifier?: Variable<string>;
+    focus_change_down?: Variable<string>;
+    focus_change_up?: Variable<string>;
+    focus_change_left?: Variable<string>;
+    focus_change_right?: Variable<string>;
+    focus_mapping?: Variable<any>; // array of focus mapping objects
+    focus_container?: Variable<any>;
+    use_last_focus?: Variable<boolean>;
+    focus_navigation_mode_left?: Variable<"none" | "stop" | "custom" | "contained">;
+    focus_navigation_mode_right?: Variable<"none" | "stop" | "custom" | "contained">;
+    focus_navigation_mode_down?: Variable<"none" | "stop" | "custom" | "contained">;
+    focus_navigation_mode_up?: Variable<"none" | "stop" | "custom" | "contained">;
+    focus_container_custom_left?: Variable<any>; // Vector of focus container custom object
+    focus_container_custom_right?: Variable<any>; // Vector of focus container custom object
+    focus_container_custom_down?: Variable<any>; // Vector of focus container custom object
+    focus_container_custom_up?: Variable<any>; // Vector of focus container custom object
+}
+
+interface SoundDefinition {
+    sound_name?: Variable<string>;
+    sound_volume?: Variable<number>;
+    sound_pitch?: Variable<number>;
+    min_seconds_between_plays?: Variable<number>;
+}
+
+interface SoundComponentProps {
+    sound_name?: Variable<string>;
+    sound_volume?: Variable<number>;
+    sound_pitch?: Variable<number>;
+    sounds?: Variable<SoundDefinition[]>;
+}
+
+interface ScreenComponentProps {
+    render_only_when_topmost?: Variable<boolean>;
+    screen_not_flushable?: Variable<boolean>;
+    always_accepts_input?: Variable<boolean>;
+    render_game_behind?: Variable<boolean>;
+    absorbs_input?: Variable<boolean>;
+    is_showing_menu?: Variable<boolean>;
+    is_modal?: Variable<boolean>;
+    should_steal_mouse?: Variable<boolean>;
+    low_frequency_rendering?: Variable<boolean>;
+    screen_draws_last?: Variable<boolean>;
+    vr_mode?: Variable<boolean>;
+    force_render_below?: Variable<boolean>;
+    send_telemetry?: Variable<boolean>;
+    close_on_player_hurt?: Variable<boolean>;
+    cache_screen?: Variable<boolean>;
+    load_screen_immediately?: Variable<boolean>;
+    gamepad_cursor?: Variable<boolean>;
+    gamepad_cursor_deflection_mode?: Variable<boolean>;
+    should_be_skipped_during_automation?: Variable<boolean>;
 }
 
 /**
@@ -341,6 +427,38 @@ function CollectionComponent<TBase extends Constructor<Control>>(Base: TBase) {
     }
 }
 
+function InputComponent<TBase extends Constructor<Control>>(Base: TBase) {
+    return class Input extends Base {
+        constructor(...args: any[]) {
+            super(...args);
+        }
+    }
+}
+
+function FocusComponent<TBase extends Constructor<Control>>(Base: TBase) {
+    return class Focus extends Base {
+        constructor(...args: any[]) {
+            super(...args);
+        }
+    }
+}
+
+function SoundComponent<TBase extends Constructor<Control>>(Base: TBase) {
+    return class Sound extends Base {
+        constructor(...args: any[]) {
+            super(...args);
+        }
+    }
+}
+
+function ScreenComponent<TBase extends Constructor<Control>>(Base: TBase) {
+    return class Screen extends Base {
+        constructor(...args: any[]) {
+            super(...args);
+        }
+    }
+}
+
 // Element type props
 export interface PanelProps extends LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
 export interface ImageProps extends SpriteComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
@@ -348,6 +466,8 @@ export interface LabelProps extends LabelComponentProps, LayoutComponentProps, C
 export interface CustomProps extends CustomRendererComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
 export interface StackPanelProps extends StackPanelComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, CollectionComponentProps, GeneratorProps {};
 export interface CollectionProps extends CollectionComponentProps, LayoutComponentProps, ControlProps, DataBindingProps, GeneratorProps {};
+export interface InputPanelProps extends InputComponentProps, FocusComponentProps, SoundComponentProps, ControlProps, LayoutComponentProps, DataBindingProps, GeneratorProps {};
+export interface ScreenProps extends ScreenComponentProps, ControlProps, LayoutComponentProps, DataBindingProps, GeneratorProps {};
 
 // Element types
 export class Label extends DataBindingComponent(LabelComponent(LayoutComponent(Control))) {
@@ -392,8 +512,21 @@ export class Collection extends CollectionComponent(DataBindingComponent(LayoutC
     }
 }
 
+export class InputPanel extends InputComponent(DataBindingComponent(FocusComponent(LayoutComponent(Control)))) {
+    constructor(props: InputPanelProps) {
+        super(undefined, props);
+        this.setType("input_panel");
+    }
+}
+
+export class Screen extends ScreenComponent(DataBindingComponent(FocusComponent(LayoutComponent(Control)))) {
+    constructor(props: ScreenProps) {
+        super(undefined, props);
+        this.setType("screen");
+    }
+}
+
 // React implementation
-import React from "npm:react";
 
 const UI_PROPS = ["panel","input_panel","image","grid","label","button","screen","scroll_box","stack_panel","toggle","slider","slider_box","dropdown","edit_box","custom","scroll_view","scroll_track","factory","selection_wheel","grid_page_indicator","label_cycler","image_cycler","collection_panel","controls","variables","visible","ignored","modifications","anims","disable_anim_fast_forward","animation_reset_name","enabled","layer","alpha","propagate_alpha","clips_children","allow_clipping","clip_offset","clip_state_change_event","enabled_scissor_test","property_bag","selected","use_child_anchors","grid_position","debug","anchor_from","anchor_to","contained","draggable","follows_cursor","offset","size","max_size","min_size","inherit_max_sibling_width","inherit_max_sibling_height","use_anchored_offset","bindings","sound_name","sound_pitch","sound_volume","default_focus_precedence","focus_enabled","focus_wrap_enabled","focus_magnet_enabled","focus_identifier","focus_change_down","focus_change_up","focus_change_left","focus_change_right","focus_mapping","focus_container","use_last_focus","focus_navigation_mode_left","focus_navigation_mode_right","focus_navigation_mode_down","focus_navigation_mode_up","focus_container_custom_left","focus_container_custom_right","focus_container_custom_down","focus_container_custom_up","button_mappings","modal","inline_modal","always_listen_to_input","always_handle_pointer","always_handle_controller_direction","hover_enabled","prevent_touch_input","consume_event","consume_hover_events","gesture_tracking_button","texture","uv","uv_size","nineslice_size","base_size","color","tiled","tiled_scale","clip_direction","clip_ratio","clip_pixelperfect","keep_ratio","bilinear","fill","grayscale","force_texture_reload","$fit_to_witdh","zip_folder","texture_file_system","allow_debug_missing_texture","pixel_perfect","grid_dimensions","maximum_grid_items","grid_dimension_binding","grid_rescaling_type","grid_fill_direction","precached_grid_item_count","grid_item_template","text","locked_color","shadow","font_size","font_scale_factor","localize","line_padding","font_type","backup_font_type","text_alignment","hide_hypen","locked_alpha","enable_profanity_filter","notify_on_ellipses","default_control","hover_control","pressed_control","locked_control","render_only_when_topmost","screen_not_flushable","always_accepts_input","render_game_behind","absorbs_input","is_showing_menu","is_modal","should_steal_mouse","low_frequency_rendering","screen_draws_last","vr_mode","force_render_below","send_telemetry","close_on_player_hurt","cache_screen","load_screen_immediately","gamepad_cursor","gamepad_cursor_deflection_mode","should_be_skipped_during_automation","orientation","radio_toggle_group","toggle_name","toggle_default_state","toggle_group_forced_index","toggle_group_default_selected","reset_on_focus_lost","toggle_on_button","toggle_off_button","enable_directional_toggling","toggle_grid_collection_name","checked_control","unchecked_control","checked_hover_control","unchecked_hover_control","checked_locked_control","unchecked_locked_control","checked_locked_hover_control","unchecked_locked_hover_control","slider_track_button","slider_small_decrease_button","slider_small_increase_button","slider_steps","slider_direction","slider_timeout","slider_collection_name","slider_name","slider_select_on_hover","slider_selected_button","slider_deselected_button","slider_box_control","background_control","background_hover_control","progress_control","progress_hover_control","indent_control","dropdown_name","dropdown_content_control","dropdown_area","text_box_name","text_edit_box_grid_collection_name","constrain_to_rect","enabled_newline","text_type","max_length","text_control","place_holder_control","can_be_deselected","always_listening","virtual_keyboard_buffer_control","renderer","scrollbar_track_button","scrollbar_touch_button","scroll_speed","gesture_control_enabled","always_handle_scrolling","touch_mode","scrollbar_box","scrollbar_track","scroll_view_port","scroll_content","scroll_box_and_track_panel","jump_to_bottom_on_update","allow_scroll_even_when_content_fits","inner_radius","outer_radius","state_controls","slice_count","button_name","iterate_left_button_name","iterate_right_button_name","initial_button_slice","grid_item_when_current","grid_item_when_not_current","cycler_manager_size_control_target","target_cycler_to_compare","next_sub_page_button_name","prev_sub_page_button_name","text_labels","images", "collection_name"];
 
@@ -404,9 +537,22 @@ export function createMinecraftElement(
 ): Control {
     properties ??= {};
 
-    // Inject 
-    if (children.length > 0) {
-        properties.children = children.length === 1 ? children[0] : children;
+    if (!Array.isArray(children)) {
+        throw new Error("Children must be an array.");
+    }
+
+    const flattenedChildren = children.flat();
+
+    flattenedChildren.forEach((child, i) => {
+        if (!(child instanceof Control)) {
+            console.error("erroring child", child);
+            throw new Error(`Child must be an instance of Control. Child at index ${i} is of type ${typeof child}. ctor: ${child.constructor.name}`);
+        }
+    });
+
+    // Inject children as properties
+    if (flattenedChildren.length > 0) {
+        properties.children = flattenedChildren;
     }
     
     const element = new controlConstructor(properties);
